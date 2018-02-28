02acf90d9d698c5b3106ccb3e96b50cadd89d655
Unknown <diogoalexsmachado@gmail.com>
2018-02-28 22:59:42 +0000

    started ws2

---
+++ ws2/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function times_table_view(){
+
+}
+
+?>
---
+++ ws2/php01.php
@@ -0,0 +1,12 @@
+<?php
+$string = "Hello world";
+$integer = 12;
+$float = 5.8;
+$boolean = 1 ;
+
+echo "
+    String: $string <br/>
+    Integer: $integer <br/>
+    Float: $float<br/>
+    Boolean: $boolean (true = 1 and false = 0)
+    ";
