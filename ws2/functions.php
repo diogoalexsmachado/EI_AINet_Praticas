b0880fa359291e46c002fb7dfafd498be65f2181
Unknown <diogoalexsmachado@gmail.com>
2018-03-01 18:57:51 +0000

    php02 fully working as requested

--- ws2/functions.php
+++ ws2/functions.php
@@ -1,7 +1,38 @@
 <?php
 
 function times_table_view(){
-
+    $isEqual = false;
+    echo "<table>";     
+    echo "<tr>";
+    echo "<td class='first'>"; 
+    echo "*";
+    echo "</td>";
+    for($b=1; $b<11;$b++){
+        echo "<td class='first'>"; 
+        echo $b;
+        echo "</td>";
+    }
+    echo "</tr>";
+    for($i = 1; $i < 11; $i++) {
+        echo "<tr>";
+        
+        echo "<td class='first'>"; 
+        echo $i;
+        echo "</td>";
+        for($a = 1; $a < 11; $a++) {
+            if($a==$i){
+                echo "<td class='equal'>"; 
+                echo $a*$i;
+                echo "</td>";
+            }else{
+                echo "<td>"; 
+                echo $a*$i;
+                echo "</td>";
+            }
+        }
+    echo "</tr>";
+    }
+    echo "</table>";    
 }
 
 ?>
--- ws2/php02.php
+++ ws2/php02.php
@@ -16,43 +16,8 @@ td { background-color:#F44336; width:35px; height:35px; color: white; text-align
     <body>
 
         <?php
-            $isEqual = false;
-            echo "<table>";     
-            echo "<tr>";
-
-            echo "<td class='first'>"; 
-            echo "*";
-            echo "</td>";
-
-            for($b=1; $b<11;$b++){
-                echo "<td class='first'>"; 
-                echo $b;
-                echo "</td>";
-            }
-
-            echo "</tr>";
-            for($i = 1; $i < 11; $i++) {
-                echo "<tr>";
-                
-                echo "<td class='first'>"; 
-                echo $i;
-                echo "</td>";
-                for($a = 1; $a < 11; $a++) {
-                    if($a==$i){
-                        echo "<td class='equal'>"; 
-                        echo $a*$i;
-                        echo "</td>";
-                    }else{
-                        echo "<td>"; 
-                        echo $a*$i;
-                        echo "</td>";
-                    }
-
-                }
-            echo "</tr>";
-            }
-            
-            echo "</table>";
+            include  "functions.php";
+            times_table_view();
         ?>
 
     </body>
